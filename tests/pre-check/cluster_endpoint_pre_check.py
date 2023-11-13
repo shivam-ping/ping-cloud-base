051e14d0652c059723b77fd8a28bb0ecbb8d8a22
Walker <walkerbrown@pingidentity.com>
2023-11-13 08:03:29 -0700

    Breaking out API call for better retry logic

--- tests/pre-check/cluster_endpoint_pre_check.py
+++ tests/pre-check/cluster_endpoint_pre_check.py
@@ -5,19 +5,22 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 
 SUBDOMAINS = ["healthcheck", "metadata"]
 
+
 class ClusterEndpointsPreCheck(unittest.TestCase):
-    @retry(stop=stop_after_attempt(3), wait=wait_fixed(5))
     def test_ingress(self):
-        dns_zone = getenv("PRIMARY_TENANT_DOMAIN", "ping-oasis.com")
+        tenant_domain = getenv("PRIMARY_TENANT_DOMAIN", "ping-oasis.com")
 
         for subdomain in SUBDOMAINS:
-            with self.subTest(msg=f"{subdomain} is not available"):
-                url = f"https://{subdomain}.{dns_zone}"
-                print(f"Checking URL: {url}")
-                try:
-                    response = requests.get(url, verify=False, timeout=5)
-                    response.raise_for_status()
-                except requests.exceptions.RequestException as e:
-                    self.fail(f"Failed to make GET request to {url}: {e}")
+            self.get_ingress_response(subdomain, tenant_domain)
 
-                self.assertEqual(response.status_code, 200)
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(5))
+    def get_ingress_response(self, subdomain, tenant_domain):
+        url = f"https://{subdomain}.{tenant_domain}"
+        print(f"Checking URL: {url}")
+        try:
+            response = requests.get(url, verify=False, timeout=5)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            self.fail(f"Failed to make GET request to {url}: {e}")
+
+        self.assertEqual(response.status_code, 200)
