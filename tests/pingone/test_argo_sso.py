8a7e8af7a6746df0aae28fa693cb3c46c3df7965
Colin Reininger <colinreininger@pingidentity.com>
2024-02-28 10:32:34 -0700

    Fix pingone app and groups exists tests

--- tests/pingone/test_argo_sso.py
+++ tests/pingone/test_argo_sso.py
@@ -17,8 +17,8 @@ class TestArgoSSO(p1_test_base.P1TestBase):
         for group_name in self.group_names:
             with self.subTest(msg=f"{group_name} created"):
                 p1_group = self.get(self.cluster_env_endpoints.groups, group_name)
-                self.assertIsNotNone(p1_group, f"Group '{group_name}' not created")
+                self.assertTrue(p1_group, f"Group '{group_name}' not created")
 
     def test_app_created(self):
         p1_app = self.get(self.cluster_env_endpoints.applications, self.app_name)
-        self.assertIsNotNone(p1_app, f"Application '{self.app_name}' not created")
+        self.assertTrue(p1_app, f"Application '{self.app_name}' not created")
--- tests/pingone/test_pf_admin_sso.py
+++ tests/pingone/test_pf_admin_sso.py
@@ -16,17 +16,17 @@ class TestPFAdminSSO(p1_test_base.P1TestBase):
             f"{self.tenant_name}-{self.environment}-pf-expression",
             f"{self.tenant_name}-{self.environment}-pf-audit",
         ]
-        self.app_name = f"client-{self.tenant_name}-pingfederate-sso"
+        self.app_name = f"client-{self.tenant_name}-pingfederate-admin-sso"
 
     def test_groups_created(self):
         for group_name in self.group_names:
             with self.subTest(msg=f"{group_name} created"):
                 p1_group = self.get(self.cluster_env_endpoints.groups, group_name)
-                self.assertIsNotNone(p1_group, f"Group '{group_name}' not created")
+                self.assertTrue(p1_group, f"Group '{group_name}' not created")
 
     def test_app_created(self):
         p1_app = self.get(self.cluster_env_endpoints.applications, self.app_name)
-        self.assertIsNotNone(p1_app, f"Application '{self.app_name}' not created")
+        self.assertTrue(p1_app, f"Application '{self.app_name}' not created")
 
 
 if __name__ == "__main__":
